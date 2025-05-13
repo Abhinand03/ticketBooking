89b1702c271a477f939d33c2f7c34c31a7e149e8
Abhinand03 <abhinandpk0123@gmail.com>
2025-05-14 00:17:13 +0530

    seo upadte

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { ssr } from 'vite-plugin-ssr/plugin';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),ssr()],
   build: {
     ssrManifest: true,
     manifest: true,
