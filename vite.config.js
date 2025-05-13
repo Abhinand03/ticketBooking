6dc96af0c3f07e754857494ad5b7746174419a0b
Abhinand03 <abhinandpk0123@gmail.com>
2025-05-14 00:09:15 +0530

    seo added

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   build: {
     ssrManifest: true,
+    manifest: true,
     rollupOptions: {
-      input: 'client/main.jsx',
+      input: path.resolve(__dirname, 'src/main.jsx'), // 🔁 Adjust this
     },
+    outDir: 'dist',
   },
 })
