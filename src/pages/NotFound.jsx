e88226dcf407d5f2d3b4b876d0689ed2453cbc35
Abhinand P K <abhinandpk0123@gmail.com>
2026-03-22 22:46:08 +0530

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,9 +6,9 @@ function NotFound() {
     <div className="flex justify-center items-center h-screen">
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-8xl font-bold">404</h1>
-      <p> Sorry, we couldn't find the page you're looking for.</p>
+      <p> Sorry, we couldndasd't find the page you're looking for.</p>
       <NavLink className="text-white px-4 py-2 rounded-3xl bg-blue-600" to={"/"}>
-        Go back to home
+        Go back todas home
       </NavLink>
     </div>
   </div>
